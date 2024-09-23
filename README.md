be8278fe338a9ed6639bf09459f645a336d0c4d8
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2024-09-23 23:15:43 +1000

    added contact handler

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ fmt is used to
 eg: v.1.0.0 -> 1 is the major version, 0 is the minor version, 0 is the patch version
 major.minor.patch 
 
+## Dynamic Reloading
+go.modd is used for dynamic reloading. However it will not show the error when the server is already running. 
--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>To get in touch, please send an email to <a href=\"mailto:vign1310@hotmail.com\">vign1310@hotmail.com")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }

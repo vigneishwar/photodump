13cbd7557a993cc35682309f67bc225cbdc11642
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2024-09-30 00:31:22 +1000

    added faq handlers

--- main.go
+++ main.go
@@ -15,9 +15,48 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>To get in touch, please send an email to <a href=\"mailto:vign1310@hotmail.com\">vign1310@hotmail.com")
 }
 
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page Not Found</h1>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<h1>FAQ</h1>
+<ul>
+	<li>
+		<b>Is there a free version?</b>
+		Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+		<b>What are your support hours?</b>
+		We have support staff answering emails 24/7, though response
+		times may be a bit slower on weekends.
+	</li>
+	<li>
+		<b>How do I contact support?</b>
+		Email us - <a href="mailto:support@photodump.com">support@photodump.com</a>
+	</li>
+</ul>`)
+	
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", contactHandler)
+	// http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/":
+			handlerFunc(w, r)
+		case "/contact":
+			contactHandler(w, r)
+		case "/faq":
+			faqHandler(w, r)
+		default:
+			pageNotFound(w, r)
+	}
+	})
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }

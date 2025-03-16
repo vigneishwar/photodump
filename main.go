2b3a62b7e69b5828c3daed31a5a45c0165a2765f
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2025-03-16 22:29:49 +1100

    added contact template and added a helper function

--- main.go
+++ main.go
@@ -5,20 +5,21 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Welcome to the homepage!"))
-    tpl, err := template.ParseFiles("/templates/home.gohtml")
+func executeTemplate(w http.ResponseWriter, filepath string) {
+    w.Header().Set("Content-Type", "text/html")
+    tpl, err := template.ParseFiles(filepath)
     if err != nil {
         log.Printf("Error parsing template: %v", err)
         http.Error(w, "there was an error parsing the template", http.StatusInternalServerError)
         return
     }
-    err = tpl.Execute(w, "Welcome to the homepage!")
+    err = tpl.Execute(w, nil)
     if err != nil {
         log.Printf("Error executing template: %v", err)
         http.Error(w, "there was an error executing the template", http.StatusInternalServerError)
@@ -26,8 +27,15 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+    tplPath := filepath.Join("templates", "home.gohtml")
+    executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Contact us at support@photodump.com"))
+    // w.Write([]byte("Contact us at support@photodump.com"))
+    tplPath := filepath.Join("templates", "contact.gohtml")
+    executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
